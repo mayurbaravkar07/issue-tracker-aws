eb7974374cc04d1d71223ce53e048ad537cad485
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-15 11:16:42 +0530

    added files

---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { Status } from '@prisma/client';
+import { Select } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { string } from 'zod';
+
+const statuses: { label: string, value?: Status }[] = [
+    { label: 'All' },
+    { label: 'Open', value: 'OPEN' },
+    { label: 'In Progress', value: 'IN_PROGRESS' },
+    { label: 'Closed', value: 'CLOSED' },
+];
+const IssueStatusFilter = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    return (
+        <Select.Root defaultValue={searchParams.get('status') || ''} onValueChange={(status) => {
+            const params = new URLSearchParams();
+            if (status)
+                params.append('status', status);
+
+            if (searchParams.get('orderBy'))
+                params.append('orderBy', searchParams.get('orderBy')!)
+
+            const query = params.size ? '?' + params.toString() : string;
+            router.push('/issues/list' + query)
+        }}>
+            <Select.Trigger placeholder='Filter by Status...' />
+            <Select.Content>
+                {statuses.map(status => (<Select.Item key={status.value} value={status.value || ''}>{status.label}</Select.Item>))}
+            </Select.Content>
+        </Select.Root>
+    )
+}
+
+export default IssueStatusFilter
